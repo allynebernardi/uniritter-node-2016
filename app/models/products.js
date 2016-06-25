6126171de1f84fd1ed1c7aa247cbc53422133379
Aline <allynebernardi@gmail.com>
2016-06-25 12:19:46 +0000

    implementar os testes de produto

--- app/models/products.js
+++ app/models/products.js
@@ -6,19 +6,13 @@ module.exports = function (server) {
     const 
         harvesterPlugin = server.plugins['hapi-harvester'],
         schema = {
-            type: 'orders',
+            type: 'products',
             attributes: {
-                total: Types.number().forbidden(),
-                created_on: Types.date().forbidden(),
-                updated_on: Types.date().forbidden(),
-                status: Types.string().forbidden().valid([ 'new', 'invoiced', 'done', 'cancelled' ]),
-                items: Types.array().items(
-                    Types.object().keys({
-                        product_id: Types.string().guid().required(),
-                        quantity: Types.number().integer().required().min(1),
-                        price: Types.number().forbidden()
-                    }).required().min(1)
-                )
+                name: Types.string().required(),
+                price: Types.Number().required(),
+                model: Types.string().forbidden(),
+                brand: Types.string().forbidden(),
+                             
             }
         }
 
